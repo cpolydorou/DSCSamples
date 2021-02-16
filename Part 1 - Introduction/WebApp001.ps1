e0f3354027018898f3a664f5667be7a2b26ddfd1
Christos Polydorou <christos.polydorou@hotmail.com>
2021-02-16 15:06:49 +0200

    Part 1
    Added Part 1 files.

---
+++ Part 1 - Introduction/WebApp001.ps1
@@ -0,0 +1,34 @@
+﻿<#
+    Author  : Christos Polydorou
+    Date    : February 16 2021
+    Purpose : DSC Configuration example
+    URL     :
+#>
+
+# This is the configuration object
+Configuration WebApp001 {
+
+    # We are importing the module that contains the 'WindowsFeature' and 'File' resources
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+
+    # This is the computer we are targeting, 'localhost' allows us to use the configuration for all machines
+    Node 'localhost' {
+
+        # Ensure that the Web-Server feature is installed and enabled.
+        WindowsFeature WebServer {
+            Ensure = "Present"
+            Name   = "Web-Server"
+        }
+
+        # Make sure that the web page file is copied to the website root folder.
+        File WebsiteContent {
+            Ensure = 'Present'
+            SourcePath = '\\fileserver\dsc\WebApp001\iisstart.htm'
+            DestinationPath = 'c:\inetpub\wwwroot'
+            Checksum = "modifiedDate"
+            Force = $true
+            MatchSource = $true
+            DependsOn = '[WindowsFeature]WebServer'
+        }
+    }
+}
