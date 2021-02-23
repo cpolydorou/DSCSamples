cd03ea15a18d423e87b1e12dca74b0c3ee2c3b1a
Christos Polydorou <christos.polydorou@hotmail.com>
2021-02-23 10:47:23 +0200

    Added Part 2 files.

---
+++ Part 2 - Local Configuration Manager/LCMClear.ps1
@@ -0,0 +1,22 @@
+﻿<#
+    Author  : Christos Polydorou
+    Date    : February 16 2021
+    Purpose : DSC LCM Configuration example - Clear the LCM configuration
+    URL     : https://blog.cpolydorou.net/2021/02/configuring-virtual-machines-using_23.html
+#>
+
+[DSCLocalConfigurationManager()]
+configuration LCMClear
+{
+    Node localhost
+    {
+        Settings
+        {
+            RefreshMode = 'Push'
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $false
+            RefreshFrequencyMins = 30
+            ConfigurationModeFrequencyMins = 15
+        }
+    }
+}
---
+++ Part 2 - Local Configuration Manager/LCMConfig.ps1
@@ -0,0 +1,22 @@
+﻿<#
+    Author  : Christos Polydorou
+    Date    : February 16 2021
+    Purpose : DSC LCM Configuration example
+    URL     : https://blog.cpolydorou.net/2021/02/configuring-virtual-machines-using_23.html
+#>
+
+[DSCLocalConfigurationManager()]
+configuration LCMConfig
+{
+    Node localhost
+    {
+        Settings
+        {
+            RefreshMode = 'Push'
+            ConfigurationMode = 'ApplyAndMonitor'
+            RebootNodeIfNeeded = $true
+            RefreshFrequencyMins = 30
+            ConfigurationModeFrequencyMins = 15
+        }
+    }
+}
