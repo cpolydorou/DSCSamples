ae67acd7276e9ab5995bda9bf4dbd48b9c144f71
Christos Polydorou <christos.polydorou@hotmail.com>
2021-05-07 10:22:29 +0300

    Part 6 - Azure Automation
    Added Part 6 configurations.

---
+++ Part 6 - Azure Automation/WebApp004.ps1
@@ -0,0 +1,23 @@
+﻿<#
+    Author  : Christos Polydorou
+    Date    : May 5 2021
+    Purpose : DSC Configuration example
+    URL     : https://blog.cpolydorou.net/2021/05/configuring-virtual-machines-using.html
+#>
+
+# This is the configuration object
+Configuration WebApp001 {
+
+    # We are importing the module that contains the 'WindowsFeature' and 'File' resources
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+
+    # This is the computer we are targeting, 'localhost' allows us to use the configuration for all machines
+    Node 'localhost' {
+
+        # Ensure that the Web-Server feature is installed and enabled.
+        WindowsFeature WebServer {
+            Ensure = "Present"
+            Name   = "Web-Server"
+        }
+    }
+}
